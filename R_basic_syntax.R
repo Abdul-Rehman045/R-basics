55e22bd91367df3a2ea754422854d2187c6c63ef
Abdul Samad <53661372+abdulsamad-optimisticstr@users.noreply.github.com>
2019-12-27 17:23:33 +0500

    incompelete

---
+++ R_basic_syntax.R
@@ -0,0 +1,72 @@
+#add two values
+a=1.6465
+b=19.05
+c=a+b
+print(c)
+#subtract two values
+a=10.3
+b=19.5
+c=a-b
+print(c)
+#multiply two values
+a=4.5
+b=1.5
+c=a+b
+print(c)
+#divide two values
+a=1
+b=1
+c=a/b
+print(c)
+#getting modulas %/%
+a=4
+b=2
+c=a%%b
+print(c)
+#incremet
+a=4
+a=a+1
+print(a)
+#decremet
+a=4
+a=a-1
+print(a)
+#Decision making and Branching
+#if statement
+a=10
+b=1
+if(a>b){
+  print("a is greater than b")
+}
+#if/else statement
+a=10
+b=1
+if(a>b){
+  print("a is greater than b")
+}else{
+  print("b is greater than a")
+}
+#if/else/elseif statement
+a=10
+b=1
+c=50
+if(a>b){
+  print("a is greatest")
+}else if(b>c){
+  print("b is greater than ca=1:5")
+  }else{
+  print("c is greatest")
+  }
+#taking input from user (readline) execute statement 1 by 1
+numb=readline(prompt="Enter Number: ")
+numb=as.integer(numb)
+print(numb)
+#LOOPS 
+#for loop
+a=c(1,2,3,4,5)
+sized=1:5
+for (i in sized) {
+  a[1]=a[1]+a[i]
+}
+print (a[1])
+
